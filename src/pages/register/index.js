75911abfe72aaa27eb9b0b47ae054f0d630ceb98
donaldnyingifa <donald.nyingifa@nxlog.org>
2023-07-03 02:49:53 +0100

    protect register

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../../UserContext';
 import { useNavigate } from "react-router-dom";
 import { generateSocialSecurityNumber } from '../../utils';
 import { database, ref, set } from '../../firebase';
@@ -6,10 +7,8 @@ import { Form, Button, Row, Col } from 'react-bootstrap';
 import './register.scss';
 
 function Register() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-}, []);
-const navigate = useNavigate();
+  
+  const navigate = useNavigate();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [dateOfBirth, setDateOfBirth] = useState('');
@@ -17,7 +16,13 @@ const navigate = useNavigate();
   const [income, setIncome] = useState('');
   const [stateOfOrigin, setStateOfOrigin] = useState('');
   const [err, setErr] = useState('');
+  const { user } = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(false);
+  const isLoggedIn = user !== null && user !== 'null' && user.length > 2;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   function writeUserData(name, email, dateOfBirth, gender, income, stateOfOrigin) {
     const userId = generateSocialSecurityNumber();
@@ -63,7 +68,7 @@ const navigate = useNavigate();
       );
 
       setErr('User added successfully');
-      setTimeout(()=>{
+      setTimeout(() => {
         navigate("/dashboard");
       }, 2000)
     } catch (err) {
@@ -82,113 +87,118 @@ const navigate = useNavigate();
 
   return (
     <>
-      <div className="register-wrapper">
-        <h1 className="center-div">Register</h1>
-        <Form onSubmit={handleRegister}>
-          <Form.Group as={Row} controlId="name">
-            <Form.Label column sm={2}>
-              Name
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </Col>
-          </Form.Group>
+      {
+        isLoggedIn && (
+          <div className="register-wrapper">
+            <h1 className="center-div">Register</h1>
+            <Form onSubmit={handleRegister}>
+              <Form.Group as={Row} controlId="name">
+                <Form.Label column sm={2}>
+                  Name
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </Col>
+              </Form.Group>
 
-          <Form.Group as={Row} controlId="email">
-            <Form.Label column sm={2}>
-              Email
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </Col>
-          </Form.Group>
+              <Form.Group as={Row} controlId="email">
+                <Form.Label column sm={2}>
+                  Email
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </Col>
+              </Form.Group>
 
-          <Form.Group as={Row} controlId="dateOfBirth">
-            <Form.Label column sm={2}>
-              Date of Birth
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="date"
-                value={dateOfBirth}
-                onChange={(e) => setDateOfBirth(e.target.value)}
-                required
-              />
-            </Col>
-          </Form.Group>
+              <Form.Group as={Row} controlId="dateOfBirth">
+                <Form.Label column sm={2}>
+                  Date of Birth
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="date"
+                    value={dateOfBirth}
+                    onChange={(e) => setDateOfBirth(e.target.value)}
+                    required
+                  />
+                </Col>
+              </Form.Group>
 
-          <Form.Group as={Row} controlId="gender">
-            <Form.Label column sm={2}>
-              Gender
-            </Form.Label>
-            <Col sm={10}>
-              {genderOptions.map((option) => (
-                <Form.Check
-                  key={option.value}
-                  inline
-                  label={option.label}
-                  type="radio"
-                  name="gender"
-                  value={option.value}
-                  checked={gender === option.value}
-                  onChange={(e) => setGender(e.target.value)}
-                  required
-                />
-              ))}
-            </Col>
-          </Form.Group>
+              <Form.Group as={Row} controlId="gender">
+                <Form.Label column sm={2}>
+                  Gender
+                </Form.Label>
+                <Col sm={10}>
+                  {genderOptions.map((option) => (
+                    <Form.Check
+                      key={option.value}
+                      inline
+                      label={option.label}
+                      type="radio"
+                      name="gender"
+                      value={option.value}
+                      checked={gender === option.value}
+                      onChange={(e) => setGender(e.target.value)}
+                      required
+                    />
+                  ))}
+                </Col>
+              </Form.Group>
 
-          <Form.Group as={Row} controlId="stateOfOrigin">
-            <Form.Label column sm={2}>
-              State of Origin
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="text"
-                value={stateOfOrigin}
-                onChange={(e) => setStateOfOrigin(e.target.value)}
-                required
-              />
-            </Col>
-          </Form.Group>
+              <Form.Group as={Row} controlId="stateOfOrigin">
+                <Form.Label column sm={2}>
+                  State of Origin
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="text"
+                    value={stateOfOrigin}
+                    onChange={(e) => setStateOfOrigin(e.target.value)}
+                    required
+                  />
+                </Col>
+              </Form.Group>
 
-          <Form.Group as={Row} controlId="income">
-            <Form.Label column sm={2}>
-              Income
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="text"
-                value={income}
-                onChange={(e) => setIncome(e.target.value)}
-                required
-              />
-            </Col>
-          </Form.Group>
+              <Form.Group as={Row} controlId="income">
+                <Form.Label column sm={2}>
+                  Income
+                </Form.Label>
+                <Col sm={10}>
+                  <Form.Control
+                    type="text"
+                    value={income}
+                    onChange={(e) => setIncome(e.target.value)}
+                    required
+                  />
+                </Col>
+              </Form.Group>
 
-          {err && (
-            <div className="center-div">
-              <p>{err}</p>
-            </div>
-          )}
+              {err && (
+                <div className="center-div">
+                  <p>{err}</p>
+                </div>
+              )}
 
-          <div className="center-div">
-            <Button type="submit" disabled={isLoading}>
-              {isLoading ? 'Loading...' : 'Register'}
-            </Button>
+              <div className="center-div">
+                <Button type="submit" disabled={isLoading}>
+                  {isLoading ? 'Loading...' : 'Register'}
+                </Button>
+              </div>
+            </Form>
           </div>
-        </Form>
-      </div>
+        )
+      }
+
     </>
   );
 }
